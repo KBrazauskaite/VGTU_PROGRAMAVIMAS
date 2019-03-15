d8e6abc6de99b9f83829325b741bddafb7e1ab48
KBrazauskaite <47447474+KBrazauskaite@users.noreply.github.com>
2019-03-15 13:36:14 +0200

    j

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    cout << "___"<< endl;
+    vector <int> pav;
+    int k;
+    for (int i=0; i<5; i++)
+    {
+        cout<<"iveskite sveikaji skaiciu: ";
+        cin>>k;
+        pav.push_back(k);
+    }
+    for (int i: pav)
+        cout<<i<<endl;
     return 0;
 }
