63ccaccb30651fd14a3da8a50994c7f3433749f7
KBrazauskaite <47447474+KBrazauskaite@users.noreply.github.com>
2019-03-21 21:56:18 +0200

    Create main2.cpp

---
+++ main2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include <vector>
+
+
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+    vector <int> pav;
+
+    int k;
+
+    for (int i=0; i<5; i++)
+
+    {
+
+        cout<<"iveskite sveikaji skaiciu: ";
+
+        cin>>k;
+
+        pav.push_back(k);
+
+    }
+
+    for (int i: pav)
+
+        cout<<i<<endl;
+
+    return 0;
+
+}
