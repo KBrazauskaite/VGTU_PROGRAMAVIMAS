bf1ec02007459d27f6b3d489b1e7e44604df270d
KBrazauskaite <47447474+KBrazauskaite@users.noreply.github.com>
2019-03-28 21:38:12 +0200

    update

--- main2.cpp
+++ main2.cpp
@@ -12,7 +12,7 @@ void tikrinimas(string &pavadinimas){ //funkcijos prototipas
     for (int i=(pavadinimas[0]>=65 && pavadinimas[0]<=90; i<n; i++){ //pagal ASCII lentele uzduodama reiksme pirmajam masyvo nariui nuo A (65) iki Z (90)        if (pavdinimas[i]>=97 &&pavadinimas[i]<=122 && pavadinimas[0]>=65 && pavadinimas[0]<=90
        if (pavadinimas[i]>=97 && pavadinimas[i]<=122 && pavadinimas[0]>=65 && pavadinimas[0]<=90) // tikrinama kad sekantys masyvo nariai butu mazosios raides nuo a (97) iki z (122)
             {
-                continue; // jei salyga tenkinama, ivedami kiti punktai
+                continue; // jei salyga tenkinama, ivedami kiti punktai;
             }
         else
                 {
