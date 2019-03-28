76a973e3f693a7cf23bf9e723935aaaccac9c4e3
KBrazauskaite <47447474+KBrazauskaite@users.noreply.github.com>
2019-03-28 21:37:15 +0200

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -6,6 +6,22 @@ using namespace std;
 struct stud {
     string var, pav;
 };
+
+void tikrinimas(string &pavadinimas){ //funkcijos prototipas
+    const x=pavadinimas.length();   // ivedama konstanta vardo ilgiui; pirmoji didzioji raide, visos kitos mazosios
+    for (int i=(pavadinimas[0]>=65 && pavadinimas[0]<=90; i<n; i++){ //pagal ASCII lentele uzduodama reiksme pirmajam masyvo nariui nuo A (65) iki Z (90)        if (pavdinimas[i]>=97 &&pavadinimas[i]<=122 && pavadinimas[0]>=65 && pavadinimas[0]<=90
+       if (pavadinimas[i]>=97 && pavadinimas[i]<=122 && pavadinimas[0]>=65 && pavadinimas[0]<=90) // tikrinama kad sekantys masyvo nariai butu mazosios raides nuo a (97) iki z (122)
+            {
+                continue; // jei salyga tenkinama, ivedami kiti punktai
+            }
+        else
+                {
+                    cout<<"neteisingai ivestas vardas!"<<endl; // jei salyga netinka i ekrana isvedama klaida ir kartojamas vardo ivedimas
+                    cout<<"iveskite studento varda: "<<endl;
+                    cin>>pavadinimas;
+                }
+            }
+        }
 int main();
     int studsk;
     cout<<"iveskite studentu skaiciu: "; // ivedamas studentu skaicius, naudojant struktura
