755f4085e824044885692e6fcc7adc9d02c0bc40
KBrazauskaite <47447474+KBrazauskaite@users.noreply.github.com>
2019-04-05 02:27:53 +0300

    Update main4.cpp

--- main4.cpp
+++ main4.cpp
@@ -1,15 +1,7 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
+
 #include "struktura.h"
 #include "funcija.h"
 
-using std::cout;
-using std::cin;
-using std::endl;
-using std::vector;
-using std::string;
 
 
 int main(){
