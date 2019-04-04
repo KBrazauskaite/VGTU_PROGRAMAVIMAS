0bcc3efe5bd856496dd8dd172a162a388a705e27
KBrazauskaite <47447474+KBrazauskaite@users.noreply.github.com>
2019-04-05 00:24:57 +0300

    Update main3.cpp

--- main3.cpp
+++ main3.cpp
@@ -18,3 +18,25 @@ struct irasas {
     void Med();
     void Rez();
 };
+
+int main(){
+    vector <irasas> duomenubaze;
+    irasas temp;
+    string temp_var, temp_pav;
+    int temp_nd, temp_egz;
+    char paklausimas;
+    do {
+    cout<<"Iveskite studento varda: "<<endl;
+    cin>>temp_var;
+    cout<<"Iveskite studento pavarde: "<<endl;
+    cin>>temp_pav;
+    cout<<"Iveskite pazymius: "<<endl;
+    cin>>temp_nd;
+    while (temp_nd !=0){
+        if (temp_nd>0 && temp_nd<11)
+        {
+            temp.paz.push_back(temp_nd);
+        }
+        else {cout<<"netinkamas pazymys, bandykite dar karta: ";}
+        cin>>temp_nd;
+    }
