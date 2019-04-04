0f949c334729b518e90ed7dfb90e5046b0716dad
KBrazauskaite <47447474+KBrazauskaite@users.noreply.github.com>
2019-04-05 02:22:41 +0300

    Update main4.cpp

--- main4.cpp
+++ main4.cpp
@@ -11,31 +11,8 @@ using std::endl;
 using std::vector;
 using std::string;
 
-struct irasas { // sukuriama programos struktura
-    string var, pav;
-    int egz, sk;
-    vector <int> paz;
-    double vid, med, rez;
-    void Vid();
-    void Med();
-    void Rez();
-};
 
 int main(){
 
  
-
- void irasas::Vid(){
-        vid=0;
-        for (int a : paz)
-            vid+=a;
-        vid=double(vid)/paz.size();
-    }
-    void irasas::Med(){
-        sort(paz.begin(), paz.end());
-        paz.size()%2==0 ? med=(paz[paz.size()/2-1]+paz[paz.size()/2])/2 : med=paz[paz.size()/2];
-    }
-    void irasas::Rez(){
-        rez=0.4*vid+0.6*egz;
-    }
 
