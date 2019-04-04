163a293c71f68e57f282dc1e1ec63fbb72eb1da2
KBrazauskaite <47447474+KBrazauskaite@users.noreply.github.com>
2019-04-05 00:22:54 +0300

    update

--- main3.cpp
+++ main3.cpp
@@ -9,7 +9,7 @@ using std::endl;
 using std::vector;
 using std::string;
 
-struct irasas {
+struct irasas { //ivedama struktura
     string var, pav;
     int egz, sk;
     vector <int> paz;
@@ -18,3 +18,25 @@ struct irasas {
     void Med();
     void Rez();
 };
+
+int main(){
+    vector <irasas> duomenubaze; //sukuriamas vektorius duomenu ivedimui
+    irasas temp;
+    string temp_var, temp_pav;
+    int temp_nd, temp_egz;
+    char paklausimas;
+    do {
+    cout<<"Iveskite studento varda: "<<endl;
+    cin>>temp_var;
+    cout<<"Iveskite studento pavarde: "<<endl;
+    cin>>temp_pav;
+    cout<<"Iveskite pazymius: "<<endl;
+    cin>>temp_nd;
+    while (temp_nd !=0){
+        if (temp_nd>0 && temp_nd<11)
+        {
+            temp.paz.push_back(temp_nd);
+        }
+        else {cout<<"netinkamas pazymys, bandykite dar karta: ";}
+        cin>>temp_nd;
+    }
