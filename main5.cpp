247cdccdcf97c7db632c5dd54a7a61823d0ee7b9
KBrazauskaite <47447474+KBrazauskaite@users.noreply.github.com>
2019-04-05 12:48:41 +0300

    Add files via upload

---
+++ main5.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+
+struct irasas { // sukuriama programos struktura
+    string var, pav;
+    int egz, sk;
+    vector <int> paz;
+    double vid, med, rez;
+    void Vid();
+    void Med();
+    void Rez();
+};
+
+int main(){
+    ifstream fr;
+    fr.open("kursiokai.txt");
+    if (inFile.fail()){ //ieskoma klaidos, ar failas egzistuoja
+        cerr<<"Neimanoma atidaryti failo!!!"<<endl;
+        exit(1);
+    }
+    string var, pav, a, b;
+    fr>>var>>pav>>a>>b;
+    fr.close();
+
+    vector <irasas> duomenubaze; // sukuriamas vektorius duomenu ivedimui
+    irasas temp;
+    string temp_var, temp_pav;
+    int temp_nd, temp_egz;
+    char paklausimas;
+    do {
+    cout<<"Iveskite studento varda: "<<endl;
+    cin>>temp_var;
+    cout<<"Iveskite studento pavarde: "<<endl;
+    cin>>temp_pav;
+    cout<<"Iveskite pazymius: "<<endl;
+    cin>>temp_nd;
+    while (temp_nd !=0){
+        if (temp_nd>0 && temp_nd<11)
+        {
+            temp.paz.push_back(temp_nd);
+        }
+        else {cout<<"netinkamas pazymys, bandykite dar karta: ";}
+        cin>>temp_nd;
+    }
+     temp.Vid(); temp.Med(); temp.Rez();
+    duomenubaze.push_back (temp);
+    cout<<"Iveskite egzamino bala: "<<endl;
+    cin>>temp_egz;
+    cout<<"ar ivesite dar? t/ kitoks kl: ";
+    cin>>paklausimas;
+    } while (paklausimas == 't');
+
+    for (auto a: duomenubaze){
+        cout<<"Vardas\t"<<"Pavarde\t"<<"Galutinis(Med.)/Galutinis(Vid.)"<<endl;
+        cout<<temp_var<<"    "<<temp_pav<<"       "<<a.med<<"        "<<a.vid<<endl;
+        cout<<"---------------------------------"<<endl;
+
+    }
+}
+
+ void irasas::Vid(){
+        vid=0;
+        for (int a : paz)
+            vid+=a;
+        vid=double(vid)/paz.size();
+    }
+    void irasas::Med(){
+        sort(paz.begin(), paz.end());
+        paz.size()%2==0 ? med=(paz[paz.size()/2-1]+paz[paz.size()/2])/2 : med=paz[paz.size()/2];
+    }
+    void irasas::Rez(){
+        rez=0.4*vid+0.6*egz;
+    }
+
