7e2d01259f3ff43495eeb097c2e1c4668ffd06dc
KBrazauskaite <47447474+KBrazauskaite@users.noreply.github.com>
2019-05-27 21:45:45 +0300

    Update calculations.h

--- calculations.h
+++ calculations.h
@@ -3,7 +3,7 @@
 #include "student.h"
 #include <vector>
 
-using std::list
-using std::deque
+using std::list;
+using std::deque;
 
 void calculate_med_avg( Student &, const std::deque<unsigned> & );
