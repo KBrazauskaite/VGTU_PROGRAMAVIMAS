fe0386331f0fededf6c78504606eeb117ca78256
KBrazauskaite <47447474+KBrazauskaite@users.noreply.github.com>
2019-05-27 21:49:42 +0300

    Update random.h

--- random.h
+++ random.h
@@ -2,5 +2,8 @@
 
 #include <vector>
 
+using std::list;
+using std::deque;
+
 void generate_random_grades( std::vector<unsigned> &, unsigned, bool );
 std::vector<unsigned> generate_random_grades( unsigned );
