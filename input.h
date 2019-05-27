54566db2bda5a7df3f29502553a686719eebceb2
KBrazauskaite <47447474+KBrazauskaite@users.noreply.github.com>
2019-05-27 21:37:08 +0300

    Update input.h

--- input.h
+++ input.h
@@ -8,6 +8,6 @@ void get_input( std::string &, const std::string & );
 void retrieve_text_input( std::string &, const std::string & );
 unsigned read_number( const std::string & );
 
-void read_from_file( std::vector<Student> & );
-void read_from_user( std::vector<Student> & );
-void read_data( std::vector<Student> & );
+void read_from_file( std::deque<Student> & );
+void read_from_user( std::deque<Student> & );
+void read_data( std::deque<Student> & );
