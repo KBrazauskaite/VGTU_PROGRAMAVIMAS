15a6bb4f7c568f667eeac46ccc9cfc347dbcdd43
KBrazauskaite <47447474+KBrazauskaite@users.noreply.github.com>
2019-05-27 21:41:01 +0300

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -7,11 +7,11 @@
 #include <algorithm>
 #include <iostream>
 
-std::vector<Student> generate_students( const unsigned n ) {
-    std::vector<Student> students;
+std::list<Student> generate_students( const unsigned n ) {
+    std::list<Student> students;
     Student s;
 
-    for ( std::vector<Student>::size_type i = 0; n != i; ++i ) {
+    for ( std::list<Student>::size_type i = 0; n != i; ++i ) {
         s.name          = "Name" + std::to_string( i + 1 );
         s.surname       = "Surname" + std::to_string( i + 1 );
         s.examScore     = std::rand() % 10 + 1;
@@ -24,13 +24,13 @@ std::vector<Student> generate_students( const unsigned n ) {
     return students;
 }
 
-const std::vector<Student> categorize_students( std::vector<Student> &students ) {
+const std::list<Student> categorize_students( std::list<Student> &students ) {
     const auto rem = std::partition( students.begin(), students.end(),
                                      []( Student &student ) {
         return student.finalScore >= 5.0;
     } );
 
-    const std::vector<Student> failed( rem, students.end() );
+    const std::list<Student> failed( rem, students.end() );
     students.erase( rem, students.cend() );
 
     return failed;
